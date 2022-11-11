335ee1950176efdd513e0c6a659b633f72d8b85c
Andrew <drew.311.a@gmail.com>
2022-11-11 16:11:17 -0500

    Setup initial repo

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.getElementById("root"));
+
+// Huzzah for jsx!
+const App = () => {
+  return <h1>Hello World</h1>
+}
+
+root.render(<App />);
